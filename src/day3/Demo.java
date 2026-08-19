699740c3cadbd8b406b4e7dedf94dcd3d54eee60
Daksh <dakshahuja31106@gmail.com>
2026-08-19 09:54:24 +0530

    session1 files

--- src/day3/Demo.java
+++ src/day3/Demo.java
@@ -1,5 +1,18 @@
 package day3;
 
 public class Demo {
+	public static void main(String[] args) {
+		
+		try {
+			int[] a = {10,20,30};
+			System.out.println(a[0]);
+			System.out.println(a[1]);
+			System.out.println(a[2]);
+			System.out.println(a[3]);
 
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("exception handeled");
+		}
+		System.out.println("Hello sir");
+	}
 }
